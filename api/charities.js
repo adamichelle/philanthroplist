cf8962ac67c83fe734cfea25a6b62f8051c3e088
Adaobi Aniuchi <adamichelllle@gmail.com>
2021-05-01 00:32:20 +0100

    Set up firebase and add function to get charities

---
+++ api/charities.js
@@ -0,0 +1,19 @@
+const firestore = require('./lib/firebase');
+
+module.exports = async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
+    const offset = (page - 1) * limit
+    try {
+        const collectionRef = firestore.collection('charities').orderBy("name").limit(limit).offset(offset);
+        const snapshot = await collectionRef.get();
+        let charities = [];
+        snapshot.forEach((doc) => {
+            let charity = doc.data();
+            charity.id = doc.id;
+            charities.push(charity)
+        });
+        res.status(200).json({ success: true, data: charities, message: "charities received" });
+    } catch (error) {
+        res.status(500).json({ success:false, data: null, message: "An error occured. Error:" + error.message })
+    }
+}
